9cd0063d3c6fbf5f7cff23bcdd77cba38ae33775
Nidhi <nidhi@instaacoders.com>
2023-10-02 17:56:02 +0530

    web View changes

--- lib/viewBlogs.dart
+++ lib/viewBlogs.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:health_wellness/constants/colors.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+// Import for Android features.
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+// Import for iOS features.
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
 class ViewBlog extends StatefulWidget {
   final String blogUrl;
@@ -12,29 +16,51 @@ class ViewBlog extends StatefulWidget {
 }
 
 class _ViewBlogState extends State<ViewBlog> {
+   late final WebViewController controller;
+
+
+   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+     controller = WebViewController()
+     ..setBackgroundColor(const Color(0x00000000))
+          ..setNavigationDelegate(
+            NavigationDelegate(
+              onProgress: (int progress) {
+                CircularProgressIndicator();
+              },
+            )
+          )
+      ..loadRequest(
+        Uri.parse('${widget.blogUrl}'),
+      );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: WebViewWidget(
-        controller: WebViewController()
-          ..setJavaScriptMode(JavaScriptMode.unrestricted)
-          ..setBackgroundColor(const Color(0x00000000))
-          ..setNavigationDelegate(
-            NavigationDelegate(
-              onProgress: (int progress) {
-                // Update loading bar.
-              },
-              onPageStarted: (String url) {},
-              onPageFinished: (String url) {},
-              onWebResourceError: (WebResourceError error) {},
-              onNavigationRequest: (NavigationRequest request) {
-                if (request.url.startsWith('${widget.blogUrl}')) {
-                  return NavigationDecision.prevent;
-                }
-                return NavigationDecision.navigate;
-              },
-            ),
-          ) ..loadRequest(Uri.parse('${widget.blogUrl}')),
+        controller: controller
+        // WebViewController()
+          // ..setJavaScriptMode(JavaScriptMode.unrestricted)
+          // ..setBackgroundColor(const Color(0x00000000))
+          // ..setNavigationDelegate(
+          //   NavigationDelegate(
+          //     onProgress: (int progress) {
+          //       // Update loading bar.
+          //     },
+          //     onPageStarted: (String url) {},
+          //     onPageFinished: (String url) {},
+          //     onWebResourceError: (WebResourceError error) {},
+          //     onNavigationRequest: (NavigationRequest request) {
+          //       if (request.url.startsWith('${widget.blogUrl}')) {
+          //         return NavigationDecision.prevent;
+          //       }
+          //       return NavigationDecision.navigate;
+          //     },
+          //   ),
+          // )
+          //  ..loadRequest(Uri.parse('${widget.blogUrl}')),
       ),
     );
   }
